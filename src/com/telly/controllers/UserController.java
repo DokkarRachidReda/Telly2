224b23e703c93404dc30001b2861d046f70a1559
Dokkar Rachid Reda <hr_dokkar@esi.dz>
2021-01-25 15:30:39 +0100

    changed user controller

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -22,15 +22,6 @@ import com.telly.service.UserService;
 @Controller
 public class UserController {
 
-	@RequestMapping("/login")
-	public String showLogin() {
-		return "login";
-	}
-	
-	@RequestMapping("/loggedout")
-	public String showLogout() {
-		return "loggedout";
-	}
 
 	@Autowired
 	UserService userService;
@@ -43,23 +34,7 @@ public class UserController {
 		return "createaccount";
 	}
 
-	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
-	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
-		
-		if(result.hasErrors()) {
-			return "createaccount";
-		}
-		
-		user.setAuthority("ROLE_USER");
-		user.setEnabled(true);
-
-		userService.create(user);
-		
-		return "home";
-
-	}
-
-
+	
 	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
 	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
 		
